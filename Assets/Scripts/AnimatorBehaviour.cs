4f1947335548a322ad359817efe7c7a3bf115ccb
ms <ms.shao@dygame.cn>
2015-01-29 10:41:32 +0800

    clone UnityAnimation project to GitHub , add file , commit , push.

---
+++ Assets/Scripts/AnimatorBehaviour.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimatorBehaviour : MonoBehaviour 
+{
+	protected Animator animator ; 
+	// Use this for initialization
+	void Start ()
+	{
+		animator = this.gameObject.GetComponent<Animator>();	
+	}
+	protected float walk ;
+	protected float direction ;
+	// Update is called once per frame
+	void Update () 
+	{
+		walk = Input.GetAxis("Vertical");
+		direction = Input.GetAxis("Horizontal");
+	}
+	//
+	void OnGUI()
+	{
+		if (GUI.Button(new Rect(10, 90 , 300, 30), "AnimatorController"))
+		{
+			//舊版Animation做法:
+/*			AnimationState anim = this.transform.animation["MainMenuButton"];
+			if( anim )
+			{
+				anim.speed = fSpeed ;
+				anim.wrapMode = WrapMode.Once ;//一次
+				anim.time = 0;
+				this.transform.animation.Play("MainMenuButton");
+			}
+*/			//新版MecAnima做法		
+			if(animator)
+			{
+				// 設定 物件上Animator變數，播放相對應動畫
+				animator.SetFloat("speed",walk);
+				animator.SetFloat("dir",direction);
+			}
+		}
+		if (GUI.Button(new Rect(10, 130 , 300, 30), "IsSameAnimatorController"))
+		{
+			//Is Playing
+			if(animator)
+			{
+				AnimatorStateInfo animatorState = animator.GetCurrentAnimatorStateInfo(0);
+				if(animatorState.IsName("Base Layer.CarAnimation"))
+				{	
+					//Debug.Log("动画相等");
+				}
+				else
+				{
+					//Debug.Log("动画不等");    
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+	//
+	protected float fSpeed = 0.9f ;
+	void OnGUI()
+	{
+		if (GUI.Button(new Rect(10, 10 , 300, 30), "MainMenuButtonAnimation"))
+		{
+			AnimationState anim = this.transform.animation["MainMenuButton"];
+			if( anim )
+			{
+				anim.speed = fSpeed ;
+				anim.time = 0;
+				anim.wrapMode = WrapMode.Once ;//一次
+				this.transform.animation.Play("MainMenuButton");
+			}
+			//20140902.沒效果? animation不能play animator?
+		  //GameObject GameObjectX = this.transform.Find("SpriteAnima").gameObject;
+		  //GameObjectX.animation.Play("CarAnimation");
+		}
+		if (GUI.Button(new Rect(10, 50 , 300, 30), "Invert MainMenuButtonAnimation"))
+		{
+			AnimationState anim = this.transform.animation["MainMenuButton"];
+			if( anim )
+			{
+				anim.speed = fSpeed * -1.0f ;//逆轉
+				anim.time = anim.length;//逆轉
+				anim.wrapMode = WrapMode.Once ;//一次
+				this.transform.animation.Play("MainMenuButton");
+			}
+		}
+	}
+}
